f7ee2d862e481e0340fa2966505bdfc2b910a172
PrasadDhale01 <prasad@crowdera.co>
2022-05-30 18:54:14 +0530

    first commit

---
+++ src/World.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+function World() {
+    return (
+        <>
+            <div className="App">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Hello World</h1>
+            </div>
+        </>
+    );
+}
+
+export default World;
